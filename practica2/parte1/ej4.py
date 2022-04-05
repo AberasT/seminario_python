3c10ddf131fefb7adf7b9e5c03bdaaa2e6b04689
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-04-05 11:29:05 -0300

    Terminado ej4 tp2

--- practica2/parte1/ej4.py
+++ practica2/parte1/ej4.py
@@ -20,33 +20,25 @@ else:
     print('-'*50 + f'\nEl titulo no esta ok, tiene {len(titulo.split())} palabras')
 
 oraciones_resumen = resumen.split('.')
-ora_faciles = 0
-ora_aceptables = 0
-ora_dificiles = 0
-ora_muy_dificiles = 0
+cantidades_oraciones = {'ora_faciles' : 0,'ora_aceptables' : 0,'ora_dificiles' : 0 ,'ora_muy_dificiles' : 0}
 
 for oracion in oraciones_resumen:
     palabras_oracion = oracion.split()
     largo = len(palabras_oracion)
 
     # HACER CON IF ELSE
-    match len(palabras_oracion):
-        case list(range(1,12)): 
-            ora_faciles += 1
-            print('Facil: ' + oracion)
-        case list(range(13,18)): 
-            ora_aceptables += 1
-            print('Aceptable: ' + oracion)
-        case list(range(18,26)): 
-            ora_dificiles += 1
-            print('Dificil: ' + oracion)
-        case _: 
-            ora_muy_dificiles += 1
-            print('Muy dificil: ' + oracion)
-print(f'''Cantidad de oraciones:
-Faciles de leer: {ora_faciles}
-Aceptables para leer: {ora_aceptables}
-Dificil de leer: {ora_dificiles}
-Muy dificil de leer: {ora_muy_dificiles}''')
+    match largo:
+        case largo if largo in range(1,12):
+            cantidades_oraciones['ora_faciles'] += 1
+        case largo if largo in range(13,18):
+            cantidades_oraciones['ora_aceptables'] += 1
+        case largo if largo in range(18,26):
+            cantidades_oraciones['ora_dificiles'] += 1
+        case largo if largo > 25: 
+            cantidades_oraciones['ora_muy_dificiles'] += 1
 
-print(list(range(18,25)))
+print(f'''Cantidad de oraciones:
+Faciles de leer: {cantidades_oraciones['ora_faciles']}
+Aceptables para leer: {cantidades_oraciones['ora_aceptables']}
+Dificil de leer: {cantidades_oraciones['ora_dificiles']}
+Muy dificil de leer: {cantidades_oraciones['ora_muy_dificiles']}''')
