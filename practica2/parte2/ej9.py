5bc5276701918047e424610602cc9c351a603f24
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-04-08 13:34:33 -0300

    Avances ej9

--- practica2/parte2/ej9.py
+++ practica2/parte2/ej9.py
@@ -7,7 +7,6 @@ celdas = [
 '*----',
 ]
 
-
 def iniciar_mapa(matriz,alto,ancho):
     """ Los espacios en los que no hay minas, los inicia en 0, para luego comenzar el conteo de cercanía """
     for i in range(alto):
@@ -24,54 +23,56 @@ def transformar_a_matriz(alto):
         celdas_matriz.append(list(celdas[i]))
     return celdas_matriz
 
-def matriz_a_string(celdas_matriz):
+def matriz_a_string(matriz):
     """ Devuelve el mapeo en matriz en forma de lista de strings, como el mapa original """
     mapa_minas = []
-    for linea in celdas_matriz:
+    for linea in matriz:
         mapa_minas.append(''.join(str(item) for item in linea))
 
 def calcular_minas():
+    """ """
     alto = len(celdas)
     ancho = len(celdas[0])
 
     celdas_matriz = transformar_a_matriz((alto,ancho))
-    
-
-    celdas_numeros = matriz_a_string()
+    celdas_matriz = sumar_cercanias(celdas_matriz,alto,ancho)
+    celdas_numeros = matriz_a_string(celdas_matriz)
 
     return celdas_numeros
 
+def sumar_cercanias(celdas,alto,ancho):
+    """"""
+    iniciar_mapa(celdas,alto,ancho)
+    for i in range(alto):
+        for j in range(ancho):
+            if celdas[i][j] == '*':
+                match(i):
+                    case 0:
+                        celdas[i+1][j] += 1
+                    case i if i == (alto-1):
+                        celdas[i-1][j] += 1
+                    case _:
+                        celdas[i+1][j] += 1
+                        celdas[i-1][j] += 1
+                match(j):
+                    case 0:
+                        celdas[i][j+1] += 1
+                    case j if j == (ancho - 1):
+                        celdas[i][j-1] += 1
+                    case _:
+                        celdas[i][j+1] += 1
+                        celdas[i][j-1] += 1
+                if (i-1) in range(alto) and (j-1) in range(ancho):
+                    if celdas[i-1][j-1] != '*':
+                        celdas[i-1][j-1] += 1
+                if (i-1) in range(alto) and (j+1) in range(ancho):
+                    if celdas[i-1][j+1] != '*':
+                        celdas[i-1][j+1] += 1
+                if (i+1) in range(alto) and (j+1) in range(ancho):
+                    if celdas[i+1][j+1] != '*':
+                        celdas[i+1][j+1] += 1
+                if (i+1) in range(alto) and (j-1) in range(ancho):
+                    if celdas[i+1][j-1] != '*':
+                        celdas[i+1][j-1] += 1
 
-for i in range(alto):
-    for j in range(ancho):
-        if celdas_matriz[i][j] == '*':
-            match(i):
-                case 0:
-                    celdas_matriz[i+1][j] += 1
-                case i if i == (alto-1):
-                    celdas_matriz[i-1][j] += 1
-                case _:
-                    celdas_matriz[i+1][j] += 1
-                    celdas_matriz[i-1][j] += 1
-            match(j):
-                case 0:
-                    celdas_matriz[i][j+1] += 1
-                case j if j == (ancho - 1):
-                    celdas_matriz[i][j-1] += 1
-                case _:
-                    celdas_matriz[i][j+1] += 1
-                    celdas_matriz[i][j-1] += 1
-            if (i-1) in range(alto) and (j-1) in range(ancho):
-                if celdas_matriz[i-1][j-1] != '*':
-                    celdas_matriz[i-1][j-1] += 1
-            if (i-1) in range(alto) and (j+1) in range(ancho):
-                if celdas_matriz[i-1][j+1] != '*':
-                    celdas_matriz[i-1][j+1] += 1
-            if (i+1) in range(alto) and (j+1) in range(ancho):
-                if celdas_matriz[i+1][j+1] != '*':
-                    celdas_matriz[i+1][j+1] += 1
-            if (i+1) in range(alto) and (j-1) in range(ancho):
-                if celdas_matriz[i+1][j-1] != '*':
-                    celdas_matriz[i+1][j-1] += 1
-
-
+print(calcular_minas())
