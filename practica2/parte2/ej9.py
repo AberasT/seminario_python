879c75d9c4d431a88c389ae03801d4cf052c5591
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-04-08 11:14:39 -0300

    Mejoras ej9 tp2

--- practica2/parte2/ej9.py
+++ practica2/parte2/ej9.py
@@ -7,19 +7,39 @@ celdas = [
 '*----',
 ]
 
-alto = len(celdas)
-ancho = len(celdas[0])
 
-celdas_matriz = []
+def iniciar_mapa(matriz,alto,ancho):
+    """ Los espacios en los que no hay minas, los inicia en 0, para luego comenzar el conteo de cercanía """
+    for i in range(alto):
+        for j in range(ancho):
+            if matriz[i][j] == '-':
+                matriz[i][j] = 0
+    return matriz
 
-for i in range(alto):
-    celdas_matriz.append(list(celdas[i]))
+def transformar_a_matriz(alto):
+    """ Devuelve el mapa de celdas original, pero en forma de matriz.
+    Asi, se puede acceder a cada elemento por separado mediante 2 indices """
+    celdas_matriz = []
+    for i in range(alto):
+        celdas_matriz.append(list(celdas[i]))
+    return celdas_matriz
 
+def matriz_a_string(celdas_matriz):
+    """ Devuelve el mapeo en matriz en forma de lista de strings, como el mapa original """
+    mapa_minas = []
+    for linea in celdas_matriz:
+        mapa_minas.append(''.join(str(item) for item in linea))
 
-for i in range(alto):
-    for j in range(ancho):
-        if celdas_matriz[i][j] == '-':
-            celdas_matriz[i][j] = 0
+def calcular_minas():
+    alto = len(celdas)
+    ancho = len(celdas[0])
+
+    celdas_matriz = transformar_a_matriz((alto,ancho))
+    
+
+    celdas_numeros = matriz_a_string()
+
+    return celdas_numeros
 
 
 for i in range(alto):
@@ -54,9 +74,4 @@ for i in range(alto):
                 if celdas_matriz[i+1][j-1] != '*':
                     celdas_matriz[i+1][j-1] += 1
 
-mapa_minas = []
 
-for linea in celdas_matriz:
-    mapa_minas.append(''.join(str(item) for item in linea))
-
-for linea in mapa_minas: print(linea)
---
+++ pruebas.py
@@ -0,0 +1,12 @@
+
+
+
+
+def cambiar(numero):
+    numero += 1
+
+
+
+num = 2
+cambiar(num)
+print(num)
