c6479e7f2110501a3c0d46a5cd050774f9c432e7
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-04-23 00:01:03 -0300

    Ej 1 tp 3

---
+++ practica3/ej1.py
@@ -0,0 +1,34 @@
+import csv
+import json
+import os
+import collections
+
+# ARCHIVOS
+ruta =  os.getcwd()
+ruta_archivo = os.path.join(ruta,"netflix_titles.csv")
+archivo_netflix = open(ruta_archivo,encoding="utf-8")
+# PARA MANIPULAR CSV
+csvreader = csv.reader(archivo_netflix,delimiter=',')
+encabezado = next(csvreader)
+print(encabezado)
+lista = [x for x in csvreader]
+
+def paisesProductores(lista):
+    paises = set()
+    for linea in lista:
+        paises_show = set(linea[5].split(','))
+        for pais in paises_show:
+            if pais != '': paises.add(pais)
+    return paises
+
+def formaParte(pais,linea):
+    return pais in linea[5]
+
+
+
+
+paises = paisesProductores(lista)
+print('*'*30)
+print(paises)
+print(len(paises))
+print('*'*30)
